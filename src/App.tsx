e6ba95226dd06cb4a58b3b6e67e65392c533f5aa
m-antic <majaantic994@gmail.com>
2022-06-24 11:01:19 +0200

    sort and filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,142 @@
+import { useEffect, useState } from 'react';
+import { Table, Layout, PageHeader } from 'antd';
+import type { ColumnsType, TablePaginationConfig, TableProps } from 'antd/lib/table';
 import './App.css';
 
-function App() {
+import articleTypes from './mocks/article-types.json';
+import { ArticleType, GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper } from './types';
+
+const fakeApiCall = (): Promise<ArticleType[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(articleTypes);
+    }, 500);
+  });
+};
+
+const App = () => {
+  const [articleTypesData, setArticleTypesData] = useState<ArticleType[] | undefined>();
+
+  const [pagination, setPagination] = useState<TablePaginationConfig>({
+    current: 1,
+    pageSize: 5,
+  });
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      const data = await fakeApiCall();
+
+      setArticleTypesData(data);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const onChange: TableProps<ArticleType>['onChange'] = (pagination) => {
+    setPagination(pagination);
+  };
+
+  const columns: ColumnsType<ArticleType> = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      sorter: (a, b) => {
+        if (a.id < b.id) {
+          return -1;
+        }
+        if (a.id > b.id) {
+          return 1;
+        }
+        return 0; //TODO refactor
+      },
+      width: '20%',
+      responsive: ['md', 'lg'],
+    },
+    {
+      title: 'Acronym',
+      dataIndex: 'acronym',
+      sorter: (a, b) => {
+        if (a.acronym.toLowerCase() < b.acronym.toLowerCase()) {
+          return -1;
+        }
+        if (a.acronym.toLowerCase() > b.acronym.toLowerCase()) {
+          return 1;
+        }
+        return 0; //TODO refactor
+      },
+      width: '20%',
+      filterMode: 'tree',
+      filterSearch: true,
+    },
+    {
+      title: 'Title DE',
+      dataIndex: 'titleDE',
+      width: '20%',
+      sorter: (a, b) => {
+        if (a.titleDE.toLowerCase() < b.titleDE.toLowerCase()) {
+          return -1;
+        }
+        if (a.titleDE.toLowerCase() > b.titleDE.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      },
+    },
+    {
+      title: 'Default Brand',
+      dataIndex: 'defaultBrand',
+      render: (brand: GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper) => brand.name,
+      width: '20%',
+      responsive: ['md', 'lg'],
+      filterMode: 'tree',
+      filterSearch: true,
+    },
+    {
+      title: 'Active',
+      dataIndex: 'isActive',
+      render: (isActive: boolean) => (isActive ? 'Yes' : 'No'),
+      width: '20%',
+      responsive: ['md', 'lg'],
+      filters: [
+        {
+          text: 'Yes',
+          value: true,
+        },
+        {
+          text: 'No',
+          value: false,
+        },
+      ],
+      onFilter: (value: string | number | boolean, record) => record.isActive === value,
+    },
+  ];
+
+  console.log('articleTypesData:', articleTypesData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="at-layout">
+      <Layout.Header>
+        <PageHeader className="at-page-header" title="Article Types Admin Panel" />
+      </Layout.Header>
+      <Layout.Content className="at-content">
+        <Table
+          className="at-table"
+          columns={columns}
+          rowKey={(record) => record.id}
+          dataSource={articleTypesData}
+          pagination={pagination}
+          loading={loading}
+          onChange={onChange}
+        />
+      </Layout.Content>
+      <Layout.Footer className="at-footer">&copy;2022 - Created by M.A.</Layout.Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,68 @@
+export interface ArticleType {
+  id: number;
+  acronym: string;
+  titleDE: string;
+  titleFR: string;
+  titleIT: string;
+  titleEN: string;
+  group: GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper;
+  defaultShippingStatus: GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper;
+  sortOrder: number;
+  isChoEnabled: boolean;
+  allowedWithDropshipper: boolean;
+  allowedWithoutDropshipper: boolean;
+  canHaveStock: boolean;
+  moreThanOneDropshipper: boolean;
+  defaultBrand: GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper;
+  canAddToOrder: boolean;
+  addOnlyIfExistsInOrder: boolean;
+  canResendArticlesInOrder: boolean;
+  isActive: boolean;
+  hasStatistic: boolean;
+  defaultDeliveryTime: DefaultDeliveryTime;
+  addToOrderPriority: number;
+  isContentBoxManageable: boolean;
+  isReadyForDeals: boolean;
+  isReadyForBundles: boolean;
+  isFrontendPage: boolean;
+  isBundle: boolean;
+  isStockManagementAllowed: boolean;
+  canCreateExperienceVoucher: boolean;
+  canCreateBoxVoucher: boolean;
+  canCreateValueVoucher: boolean;
+  canAddToOrderOnlyWithArticleType?: (CanAddToOrderOnlyWithArticleTypeEntity | null)[] | null;
+  bundlePlusDifferenceArticle?: BundlePlusDifferenceArticleOrBundleMinusDifferenceArticle | null;
+  bundleMinusDifferenceArticle?: BundlePlusDifferenceArticleOrBundleMinusDifferenceArticle1 | null;
+  canHaveZeroPrice: boolean;
+  defaultDropshipper?: GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper1 | null;
+}
+
+export interface GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper {
+  id: number;
+  name: string;
+}
+
+export interface DefaultDeliveryTime {
+  id: number;
+  titleDE: string;
+}
+
+export interface CanAddToOrderOnlyWithArticleTypeEntity {
+  id: number;
+  acronym: string;
+}
+
+export interface BundlePlusDifferenceArticleOrBundleMinusDifferenceArticle {
+  titleWithArticleNumber: string;
+  id: number;
+}
+
+export interface BundlePlusDifferenceArticleOrBundleMinusDifferenceArticle1 {
+  titleWithArticleNumber: string;
+  id: number;
+}
+
+export interface GroupOrDefaultShippingStatusOrDefaultBrandOrDefaultDropshipper1 {
+  id: number;
+  name: string;
+}
